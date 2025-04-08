ec8382b36a0fd58fabb6ad2f47343b7427952262
Midhun <42476012+pmidhun1728@users.noreply.github.com>
2025-04-08 13:55:18 -0400

    ReverseLinkedList

---
+++ src/test/java/List/ReverseLinkedList.java
@@ -0,0 +1,46 @@
+package List;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class ReverseLinkedList {
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+    public static void printList(ListNode head) {
+        for (ListNode temp = head; temp != null; temp = temp.next) {
+            System.out.print(temp.val + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original list:");
+        printList(head);
+
+        head = reverseList(head);
+
+        System.out.println("Reversed list:");
+        printList(head);
+    }
+}
