d07a4b9eb543312256795d32f3da09b647fbbfcf
Midhun <42476012+pmidhun1728@users.noreply.github.com>
2025-04-12 23:02:00 -0400

    Stream update

--- src/test/java/Streams/SecondHighestSalary.java
+++ src/test/java/Streams/SecondHighestSalary.java
@@ -5,6 +5,12 @@ import java.util.*;
 public class SecondHighestSalary {
     public static void main(String[] args) {
         List<Integer> salaries = Arrays.asList(5000, 7000, 12000, 7000, 10000, 12000, 9000);
-        
+
+        Optional<Integer> secondHighest = salaries.stream()
+                .distinct()
+                .sorted(Comparator.reverseOrder())
+                .skip(1)
+                .findFirst();
+
     }
 }
