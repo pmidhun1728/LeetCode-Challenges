46973c382d3f00545fdba5bf1b3b4491635a1f18
Midhun <42476012+pmidhun1728@users.noreply.github.com>
2025-04-12 23:01:35 -0400

    SecondHighestSalary

---
+++ src/test/java/Streams/SecondHighestSalary.java
@@ -0,0 +1,10 @@
+package Streams;
+
+import java.util.*;
+
+public class SecondHighestSalary {
+    public static void main(String[] args) {
+        List<Integer> salaries = Arrays.asList(5000, 7000, 12000, 7000, 10000, 12000, 9000);
+        
+    }
+}
