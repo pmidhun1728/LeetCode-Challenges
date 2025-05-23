08db7a004b8106ae5afb1de1322b0e12114d7678
Midhun <42476012+pmidhun1728@users.noreply.github.com>
2025-05-22 22:12:08 -0400

    main method Scanner class added

--- src/test/java/sort/PrimeChecker.java
+++ src/test/java/sort/PrimeChecker.java
@@ -1,5 +1,7 @@
 package sort;
 
+import java.util.Scanner;
+
 public class PrimeChecker {
 
     public static boolean isPrime(int number) {
@@ -16,12 +18,14 @@ public class PrimeChecker {
     }
 
     public static void main(String[] args) {
-        int input = 29;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the Scanner Number: ");
+        int scannerInt = scanner.nextInt();
 
-        if (isPrime(input)) {
-            System.out.println(input + " is a prime number.");
+        if (isPrime(scannerInt)) {
+            System.out.println(scannerInt + " is a prime number.");
         } else {
-            System.out.println(input + " is not a prime number.");
+            System.out.println(scannerInt + " is not a prime number.");
         }
     }
 }
