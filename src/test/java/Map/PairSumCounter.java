a6815da066f9d00bf93a20797d90c998d2b3676e
Midhun <42476012+pmidhun1728@users.noreply.github.com>
2025-05-22 22:17:13 -0400

    countPairsWithSum

--- src/test/java/Map/PairSumCounter.java
+++ src/test/java/Map/PairSumCounter.java
@@ -1,4 +1,25 @@
 package Map;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PairSumCounter {
+
+    public static int countPairsWithSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+
+        for (int num : nums) {
+            int complement = target - num;
+            if (map.containsKey(complement)) {
+                count += map.get(complement);
+            }
+
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        return count;
+    }
+
+   
 }
