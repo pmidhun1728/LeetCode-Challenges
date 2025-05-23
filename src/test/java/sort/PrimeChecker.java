b0b43b54cbebb43c2175cfee178704a625a82abb
Midhun <42476012+pmidhun1728@users.noreply.github.com>
2025-05-22 22:09:04 -0400

    IsPrime method

--- src/test/java/sort/PrimeChecker.java
+++ src/test/java/sort/PrimeChecker.java
@@ -2,6 +2,19 @@ package sort;
 
 public class PrimeChecker {
 
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
 }
 
